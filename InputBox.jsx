3761bef29dfc2a85f3babaaa47f58a0b25bcf676
hashanCB <45811945+hashanCB@users.noreply.github.com>
2024-01-19 21:13:19 +0000

    Update InputBox.jsx
    modified code with comments, cleaner structure, and some tips for easy modification and reuse

--- InputBox.jsx
+++ InputBox.jsx
@@ -1,39 +1,43 @@
-import React from "react";
-import { useState } from "react";
+import React, { useState } from "react";
 
-const InputBox = () => {
-  const [showPlaceholder, setShowPlaceholder] = useState(false);
-
+// Define the InputBox component
+const InputBox = ({ placeholderText }) => {
+  // State to manage focus and placeholder display
   const [focus, setFocus] = useState(false);
+
+  // CSS classes for styling
+  const inputBoxFullStyles =
+    "relative bg-[#e9ebf0] p-3 rounded-lg border-b-[3px] w-full focus:border-black focus:outline-none";
+  const inputBoxStyles =
+    "bg-[#e9ebf0] w-full rounded-lg focus:outline-none";
+
+  // Event handlers for focus and blur
+  const handleFocus = () => setFocus(true);
+  const handleBlur = () => setFocus(false);
 
   return (
     <div
-      className={`relative bg-[#e9ebf0]  p-3   rounded-lg border-b-[3px] w-full 
-      focus:border-black
-      focus:outline-none
-      ${focus ? "border-black" : ""}
-      ${focus ? "hover:border-black" : "hover:border-[#9d9b9b]"}
-      `}
-      onFocus={() => setFocus(true)}
-      onBlur={() => setFocus(false)}
+      className={`${inputBoxFullStyles} ${
+        focus ? "border-black hover:border-black" : "hover:border-[#9d9b9b]"
+      }`}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
     >
+      {/* Input field */}
       <input
         type="email"
-        onFocus={() => setShowPlaceholder(true)}
-        onBlur={() => setShowPlaceholder(false)}
-        className="bg-[#e9ebf0] w-full  rounded-lg focus:outline-none 
-                          "
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        className={inputBoxStyles}
       />
+
+      {/* Placeholder animation */}
       <label
-        className={`text-[#5f6166]  absolute duration-300 top-3 left-3 
-                            ${
-                              showPlaceholder
-                                ? "top-1 left-3 text-[11px]"
-                                : "top-3 left-3"
-                            }
-                            `}
+        className={`text-[#5f6166] absolute duration-300 top-${
+          focus ? "1" : "3"
+        } left-3 ${focus ? "text-[11px]" : ""}`}
       >
-        Email
+        {placeholderText}
       </label>
     </div>
   );
